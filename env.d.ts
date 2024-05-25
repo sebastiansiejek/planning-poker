000b4078f7d3f3b402af588649f17bf9bb793373
Sebastian Siejek <s.siejek@timecamp.com>
2024-05-25 13:36:09 +0200

    chore: add env vars
    Signed-off-by: Sebastian Siejek <s.siejek@timecamp.com>

---
+++ env.d.ts
@@ -0,0 +1,8 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    PUSHER_APP_ID: string;
+    NEXT_PUBLIC_PUSHER_KEY: string;
+    PUSHER_SECRET: string;
+    NEXT_PUBLIC_PUSHER_CLUSTER: string;
+  }
+}
