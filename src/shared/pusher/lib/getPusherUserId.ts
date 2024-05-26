064c5cd12f48ecff21575e42b0eacd0d5d96bda5
Sebastian Siejek <s.siejek@timecamp.com>
2024-05-26 22:11:52 +0200

    feat: add getPusherUserId
    Signed-off-by: Sebastian Siejek <s.siejek@timecamp.com>

---
+++ src/shared/pusher/lib/getPusherUserId.ts
@@ -0,0 +1,18 @@
+import Cookie from "js-cookie";
+import {nanoid} from "nanoid";
+
+export const PUSHER_USER_ID_COOKIE = "PUSHER_USER_ID";
+
+export const getPusherUserId = () => {
+  const storedId = Cookie.get(PUSHER_USER_ID_COOKIE);
+
+  if (storedId) {
+    return storedId;
+  }
+
+  if (!storedId) {
+    const id = nanoid()
+    Cookie.set(PUSHER_USER_ID_COOKIE, id)
+    return id
+  }
+}
