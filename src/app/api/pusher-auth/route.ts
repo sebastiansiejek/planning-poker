54efebb07e272f122ca33bb2bc4fa23406f0493f
Sebastian Siejek <s.siejek@timecamp.com>
2024-05-26 20:54:58 +0200

    feat: add auth routing
    Signed-off-by: Sebastian Siejek <s.siejek@timecamp.com>

---
+++ src/app/api/pusher-auth/route.ts
@@ -0,0 +1,32 @@
+import {nanoid} from "nanoid"
+import {pusherServer} from "@/shared/pusher/lib/pusherServer";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export async function POST(req: Request) {
+  const formData = await req.formData()
+  // TODO: add zod validation
+  const socketId = formData.get("socket_id") as string
+  const channelName = formData.get("channel_name") as string
+  const user = JSON.parse(formData.get("userInfo") as string)
+
+  const user_id = nanoid()
+
+  const auth = pusherServer.authorizeChannel(
+    socketId,
+    channelName,
+    {
+      user_id: user_id,
+      user_info: {
+        user_id,
+        ...user
+      }
+    }
+  )
+
+  return Response.json(auth)
+}
