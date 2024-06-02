053da4fb23a1c7b878addded9619f1c06be006c5
Sebastian Siejek <s.siejek@timecamp.com>
2024-06-02 17:24:18 +0200

    fix: tailwind darkmode selector
    Signed-off-by: Sebastian Siejek <s.siejek@timecamp.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  darkMode: ['selector', '[data-theme="dark"]'],
   theme: {
     colors: {
       white: '#fff',
