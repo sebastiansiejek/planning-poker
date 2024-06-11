3eb521e5b9296a2b8ab78b07d48736bb8375acfc
Sebastian Siejek <s.siejek@timecamp.com>
2024-06-11 20:55:22 +0200

    feat: add homepage
    Signed-off-by: Sebastian Siejek <s.siejek@timecamp.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
-export default function Home() {
+import Link from 'next/link';
+import { getTranslations } from 'next-intl/server';
+
+import { routes } from '@/shared/routes/routes';
+import { Button } from '@/shared/UIKit/Button/Button';
+
+export default async function Home() {
+  const t = await getTranslations();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-semibold">Planning Poker</h1>
+    <main className="container grid md:grid-cols-2 gap-6 h-svh items-center py-6">
+      <div className="grid gap-4">
+        <h1 className="text-4xl font-semibold">{t('Homepage.title')}</h1>
+        <p className="text-balance whitespace-pre-line">
+          {t('Homepage.description')}
+        </p>
+        <div className="mt-2">
+          <Link href={routes.game.getPath()}>
+            <Button>{t('Game.submitButton.label')}</Button>
+          </Link>
+        </div>
+      </div>
+      <div>
+        <video src="/sample-game.webm" autoPlay loop muted playsInline />
+      </div>
     </main>
   );
 }
