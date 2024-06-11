78a194d3214781778985465781d8b3e94be71cc5
Sebastian Siejek <s.siejek@timecamp.com>
2024-06-11 18:01:41 +0200

    fix: redirect error
    Signed-off-by: Sebastian Siejek <s.siejek@timecamp.com>

--- src/app/actions/createOrJoinToRoom.ts
+++ src/app/actions/createOrJoinToRoom.ts
@@ -1,16 +1,11 @@
 'use server';
 
 import { redirect } from 'next/navigation';
-import z from 'zod';
 
 export const createOrJoinToRoom = async (formData: FormData) => {
   const name = formData.get('name');
 
-  try {
-    z.string().parse(name);
-
+  if (name) {
     redirect(`/game/${name}`);
-  } catch (error) {
-    console.error(error);
   }
 };
