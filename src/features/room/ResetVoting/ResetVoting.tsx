52898031b8025dc44728b82215dc15229c850800
sebastiansiejek <siejeksebastian@gmail.com>
2024-06-12 08:11:40 +0200

    feat: add missing translations (#17)
    Signed-off-by: Sebastian Siejek <s.siejek@timecamp.com>

--- src/features/room/ResetVoting/ResetVoting.tsx
+++ src/features/room/ResetVoting/ResetVoting.tsx
@@ -1,12 +1,16 @@
+import { useTranslations } from 'next-intl';
+
 import { resetVotes } from '@/app/actions/resetVotes';
 import { Button } from '@/shared/UIKit/Button/Button';
 
 export const ResetVoting = ({ channelName }: { channelName: string }) => {
+  const t = useTranslations('Room');
+
   return (
     <form action={resetVotes}>
       <input type="hidden" name="channelName" defaultValue={channelName} />
       <Button type="submit" variant="secondary">
-        Reset
+        {t('reset.button')}
       </Button>
     </form>
   );
