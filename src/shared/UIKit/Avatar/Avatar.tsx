301adf3a910564e359a343268a8f40c1c62edf40
Sebastian Siejek <s.siejek@timecamp.com>
2024-09-01 13:12:18 +0200

    feat: add loader state to avatar
    Signed-off-by: Sebastian Siejek <s.siejek@timecamp.com>

--- src/shared/UIKit/Avatar/Avatar.tsx
+++ src/shared/UIKit/Avatar/Avatar.tsx
@@ -1,8 +1,16 @@
 import Image from 'next/image';
+import Skeleton from 'react-loading-skeleton';
 
 import type { UserAvatarProps } from '@/shared/UIKit/Avatar/types';
+import type { ILoader } from '@/types/types';
 
-export const Avatar = ({ url, size = 80 }: UserAvatarProps) => {
+export const Avatar = ({
+  url,
+  size = 80,
+  isLoading,
+}: UserAvatarProps & ILoader) => {
+  if (isLoading) return <Skeleton circle width={size} height={size} />;
+
   return (
     <Image
       src={url}
