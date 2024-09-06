ae25dc0714731a2bba893007752405055e427be8
Sebastian Siejek <siejeksebastian@gmail.com>
2024-09-06 18:25:56 +0200

    feat: add roundHalf function
    Signed-off-by: Sebastian Siejek <siejeksebastian@gmail.com>

---
+++ src/shared/utils/roundHalf/roundHalf.test.ts
@@ -0,0 +1,16 @@
+import { roundHalf } from './roundHalf';
+
+describe('roundHalf', () => {
+  it('should round to the nearest half', () => {
+    expect(roundHalf(0.8)).toBe(1);
+    expect(roundHalf(1.1)).toBe(1);
+    expect(roundHalf(1.211111)).toBe(1);
+    expect(roundHalf(1.299999)).toBe(1.5);
+    expect(roundHalf(1.4)).toBe(1.5);
+    expect(roundHalf(1.5)).toBe(1.5);
+    expect(roundHalf(1.6)).toBe(1.5);
+    expect(roundHalf(1.7)).toBe(1.5);
+    expect(roundHalf(1.8)).toBe(2);
+    expect(roundHalf(1.9)).toBe(2);
+  });
+});
---
+++ src/shared/utils/roundHalf/roundHalf.ts
@@ -0,0 +1,1 @@
+export const roundHalf = (num: number) => Math.round(num * 2) / 2;
