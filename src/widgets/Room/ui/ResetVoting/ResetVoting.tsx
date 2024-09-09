e2bc766b02fa96c5ca5e961f21222572f802cb02
Sebastian Siejek <siejeksebastian@gmail.com>
2024-09-09 21:16:07 +0200

    Revert "refactor: rename useCounter with useCountdown"
    This reverts commit 38c5f4109d7d1af4f4f91184e9b3ed8c77af011b.

--- src/widgets/Room/ui/ResetVoting/ResetVoting.tsx
+++ src/widgets/Room/ui/ResetVoting/ResetVoting.tsx
@@ -1,13 +1,13 @@
 import { useTranslations } from 'next-intl';
 
-import { useCountdown } from '@/shared/hooks/useCountdown/useCountdown';
+import { useCounter } from '@/shared/hooks/useCounter/useCounter';
 import { Button } from '@/shared/UIKit/Button/Button';
 import { resetVotes } from '@/widgets/Room/actions/resetVotes';
 import type { ResetVotingProps } from '@/widgets/Room/ui/ResetVoting/ResetVoting.types';
 
 export const ResetVoting = ({ channelName }: ResetVotingProps) => {
   const t = useTranslations('Room');
-  const { counter } = useCountdown({ time: 3000 });
+  const { counter } = useCounter({ time: 3000 });
   const isCounter = counter > 0;
 
   return (
