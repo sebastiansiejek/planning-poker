6ef6afa056ba988e57a00fa630a593a76079517a
Sebastian Siejek <siejeksebastian@gmail.com>
2024-09-09 21:30:52 +0200

    Revert "Revert "feat: disable reset voting button until countdown is enabled""
    This reverts commit 9f5ba5338929fee2657ad9ad5bed46eda548d4d5.

--- src/widgets/Room/ui/ResetVoting/ResetVoting.tsx
+++ src/widgets/Room/ui/ResetVoting/ResetVoting.tsx
@@ -1,16 +1,22 @@
 import { useTranslations } from 'next-intl';
 
+import { useCounter } from '@/shared/hooks/useCounter/useCounter';
 import { Button } from '@/shared/UIKit/Button/Button';
 import { resetVotes } from '@/widgets/Room/actions/resetVotes';
+import type { ResetVotingProps } from '@/widgets/Room/ui/ResetVoting/ResetVoting.types';
 
-export const ResetVoting = ({ channelName }: { channelName: string }) => {
+export const ResetVoting = ({ channelName }: ResetVotingProps) => {
   const t = useTranslations('Room');
+  const { counter } = useCounter({ time: 3000 });
+  const isCounter = counter > 0;
 
   return (
     <form action={resetVotes}>
       <input type="hidden" name="channelName" defaultValue={channelName} />
-      <Button type="submit" variant="secondary">
-        {t('reset.button')}
+      <Button type="submit" variant="secondary" disabled={isCounter}>
+        <span>
+          {t('reset.button')} {isCounter && `(${counter})`}
+        </span>
       </Button>
     </form>
   );
