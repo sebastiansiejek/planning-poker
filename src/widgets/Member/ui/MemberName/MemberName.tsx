22ca6901c519acdb480d285fe469581de21f3e2a
Sebastian Siejek <siejeksebastian@gmail.com>
2024-09-14 11:00:46 +0200

    feat: cut member name to one line
    Signed-off-by: Sebastian Siejek <siejeksebastian@gmail.com>

--- src/widgets/Member/ui/MemberName/MemberName.tsx
+++ src/widgets/Member/ui/MemberName/MemberName.tsx
@@ -1,10 +1,21 @@
+'use client';
+
+import { useRef } from 'react';
 import Skeleton from 'react-loading-skeleton';
 
+import { useIsEllipsisActive } from '@/shared/hooks/useIsEllipsisActive/useIsEllipsisActive';
 import type { MemberNameProps } from '@/widgets/Member/ui/MemberName/types';
 
 export const MemberName = ({ name, isLoading }: MemberNameProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const isEllipsisActive = useIsEllipsisActive(ref);
+
   return (
-    <div className="font-semibold mt-2">
+    <div
+      className="font-semibold mt-2 line-clamp-1"
+      ref={ref}
+      {...(isEllipsisActive && { title: name })}
+    >
       {isLoading ? <Skeleton height={12} width={80} /> : name}
     </div>
   );
