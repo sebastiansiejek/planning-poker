50b013af7c34011ac6c43dc5f9e963e3d6f9c1b7
Sebastian Siejek <siejeksebastian@gmail.com>
2024-09-27 18:07:47 +0200

    feat: add TextInput
    Signed-off-by: Sebastian Siejek <siejeksebastian@gmail.com>

--- src/shared/UIKit/TextInput/TextInput.tsx
+++ src/shared/UIKit/TextInput/TextInput.tsx
@@ -1,12 +1,40 @@
-import type { DetailedHTMLProps, InputHTMLAttributes } from 'react';
+import { Input } from '@headlessui/react';
+import { ErrorMessage } from '@hookform/error-message';
 
-export const TextInput = (
-  props: DetailedHTMLProps<
-    InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement
-  >,
-) => {
+import { FormField } from '@/shared/UIKit/Form/FormField/FormField';
+import { FormFieldLabel } from '@/shared/UIKit/Form/FormFieldLabel/FormFieldLabel';
+import type { TextInputProps } from '@/shared/UIKit/TextInput/TextInput.types';
+import { renderClass } from '@/shared/utils/renderClass/renderClass';
+
+export const TextInput = (props: TextInputProps) => {
+  const { name } = props;
+  const { innerRef, errors, className, label, ...rest } = props;
+
   return (
-    <input className="border border-solid border-primary-500 p-2" {...props} />
+    <FormField>
+      {label && <FormFieldLabel>{label}</FormFieldLabel>}
+      <Input
+        className={renderClass(
+          'border border-solid border-primary-500 p-2 w-full',
+          {
+            'border-red-500': errors?.[name],
+            [className || '']: className,
+          },
+        )}
+        ref={innerRef}
+        {...rest}
+      />
+      <ErrorMessage
+        errors={errors}
+        name={name}
+        render={({ message }) => {
+          return (
+            <div className="text-red-500 mt-2">
+              <small>{message}</small>
+            </div>
+          );
+        }}
+      />
+    </FormField>
   );
 };
