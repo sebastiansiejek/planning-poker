06d6b75bce183276fc1a868fc702d092ba23b60c
Sebastian Siejek <siejeksebastian@gmail.com>
2024-09-27 18:58:59 +0200

    refactor: separate FormField from Input
    Signed-off-by: Sebastian Siejek <siejeksebastian@gmail.com>

--- src/shared/UIKit/TextInput/TextInput.tsx
+++ src/shared/UIKit/TextInput/TextInput.tsx
@@ -1,26 +1,23 @@
 import { Input } from '@headlessui/react';
 
-import { FormField } from '@/shared/UIKit/Form/FormField/FormField';
 import type { TextInputProps } from '@/shared/UIKit/TextInput/TextInput.types';
 import { renderClass } from '@/shared/utils/renderClass/renderClass';
 
 export const TextInput = (props: TextInputProps) => {
   const { name } = props;
-  const { innerRef, errors, className, label, ...rest } = props;
+  const { innerRef, errors, className, ...rest } = props;
 
   return (
-    <FormField name={name} label={label}>
-      <Input
-        className={renderClass(
-          'border border-solid border-primary-500 p-2 w-full',
-          {
-            'border-red-500': errors?.[name],
-            [className || '']: className,
-          },
-        )}
-        ref={innerRef}
-        {...rest}
-      />
-    </FormField>
+    <Input
+      className={renderClass(
+        'border border-solid border-primary-500 p-2 w-full',
+        {
+          'border-red-500': errors?.[name],
+          [className || '']: className,
+        },
+      )}
+      ref={innerRef}
+      {...rest}
+    />
   );
 };
