d87bd9c478db907f073ac3b3dabc717d13324fbd
Sebastian Siejek <siejeksebastian@gmail.com>
2024-10-14 18:24:54 +0200

    refactor: adjust Skeleton to shadcn
    Signed-off-by: Sebastian Siejek <siejeksebastian@gmail.com>

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,21 +1,20 @@
-import Skeleton from 'react-loading-skeleton';
-
 import { Container } from '@/shared/UIKit/Container/Container';
+import { Skeleton } from '@/shared/UIKit/Skeleton/Skeleton';
 
 export default function Loading() {
   return (
     <Container>
       <div className="grid md:grid-cols-2 gap-8 h-svh items-center">
         <div className="grid gap-4">
-          <Skeleton height={100} />
-          <Skeleton height={50} />
-          <Skeleton height={50} />
-          <Skeleton height={50} />
-          <Skeleton height={50} />
-          <Skeleton height={100} />
+          <Skeleton className="h-[10rem]" />
+          <Skeleton className="h-[5rem]" />
+          <Skeleton className="h-[5rem]" />
+          <Skeleton className="h-[5rem]" />
+          <Skeleton className="h-[5rem]" />
+          <Skeleton className="h-[10rem]" />
         </div>
         <div>
-          <Skeleton height={400} />
+          <Skeleton className="h-[40rem]" />
         </div>
       </div>
     </Container>
