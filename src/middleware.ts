327483bd1b9c1d4e0e1a4c5dcf17bb48e6e5a707
Sebastian Siejek <siejeksebastian@gmail.com>
2024-10-14 14:44:50 +0200

    feat: add /game:path* to auth routes
    Signed-off-by: Sebastian Siejek <siejeksebastian@gmail.com>

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,32 +6,28 @@ import { NextResponse } from 'next/server';
 import { ApiSessionClient } from '@/shared/api/session/ApiSessionClient';
 import { routes } from '@/shared/routes/routes';
 
-export const config = { matcher: ['/dashboard/:path*'] };
+export const config = { matcher: ['/dashboard/:path*', '/game/:path*'] };
 
 // eslint-disable-next-line func-names,import/no-anonymous-default-export
 export default async function (request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith(routes.dashboard.getPath())) {
-    const sessionCookie = request.cookies.get('next-auth.session-token')?.value;
+  const sessionCookie = request.cookies.get('next-auth.session-token')?.value;
 
-    if (sessionCookie) {
-      const apiSessionClient = new ApiSessionClient();
-      const sessionResponse = await apiSessionClient.getSession({
-        cookie: request.headers.get('cookie') || '',
-        url: request.nextUrl.origin,
-      });
+  if (sessionCookie) {
+    const apiSessionClient = new ApiSessionClient();
+    const sessionResponse = await apiSessionClient.getSession({
+      cookie: request.headers.get('cookie') || '',
+      url: request.nextUrl.origin,
+    });
 
-      if (sessionResponse.status !== 200) {
-        return NextResponse.redirect(
-          new URL(routes.login.getPath(), request.url),
-        );
-      }
-    }
-
-    if (!sessionCookie) {
+    if (sessionResponse.status !== 200) {
       return NextResponse.redirect(
         new URL(routes.login.getPath(), request.url),
       );
     }
+  }
+
+  if (!sessionCookie) {
+    return NextResponse.redirect(new URL(routes.login.getPath(), request.url));
   }
 
   return NextResponse.next();
