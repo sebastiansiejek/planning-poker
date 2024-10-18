9f4ecd951d7e9c6e26f0406d4a5abd5647472e45
Sebastian Siejek <siejeksebastian@gmail.com>
2024-10-18 17:24:33 +0200

    feat: add Table
    Signed-off-by: Sebastian Siejek <siejeksebastian@gmail.com>

--- src/shared/UIKit/Button/types.ts
+++ src/shared/UIKit/Button/types.ts
@@ -1,8 +1,11 @@
-import type { ButtonHTMLAttributes, ReactNode } from 'react';
+import type { VariantProps } from 'class-variance-authority';
+import type { ButtonHTMLAttributes } from 'react';
 
-export type ButtonProps = {
-  children: ReactNode;
-  variant?: 'primary' | 'secondary' | 'text';
+import type { buttonVariants } from '@/shared/UIKit/Button/Button';
+
+export interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
   isLoading?: boolean;
-  color?: 'primary' | 'danger';
-} & ButtonHTMLAttributes<HTMLButtonElement>;
+}
