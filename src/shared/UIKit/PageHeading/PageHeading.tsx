203a29057288f803d2e009c929e10afcde037e1d
Sebastian Siejek <siejeksebastian@gmail.com>
2024-10-19 15:41:25 +0200

    feat: set uppercase of page heading title
    Signed-off-by: Sebastian Siejek <siejeksebastian@gmail.com>

--- src/shared/UIKit/PageHeading/PageHeading.tsx
+++ src/shared/UIKit/PageHeading/PageHeading.tsx
@@ -8,7 +8,7 @@ type PageHeadingProps = {
 export const PageHeading = ({ title }: PageHeadingProps) => {
   return (
     <div className="mx-auto text-center pt-3 lg:pt-10 pb-10 lg:pb-20 max-w-5xl">
-      <Heading variant="h1" htmlAttributes={{ className: 'pb-5' }}>
+      <Heading variant="h1" htmlAttributes={{ className: 'pb-5 uppercase' }}>
         {title}
       </Heading>
       <Separator />
