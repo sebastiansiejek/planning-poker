a1491f2e38c8b3e0fb1e4c4604eaa42632cbd765
Sebastian Siejek <siejeksebastian@gmail.com>
2024-10-20 12:20:50 +0200

    feat: add description to PageHeading
    Signed-off-by: Sebastian Siejek <siejeksebastian@gmail.com>

--- src/shared/UIKit/PageHeading/PageHeading.tsx
+++ src/shared/UIKit/PageHeading/PageHeading.tsx
@@ -1,16 +1,19 @@
 import { Heading } from '@/shared/UIKit/Heading/Heading';
+import { Paragraph } from '@/shared/UIKit/Paragraph/Paragraph';
 import { Separator } from '@/shared/UIKit/Separator/Separator';
 
 type PageHeadingProps = {
   title: string;
+  description?: string;
 };
 
-export const PageHeading = ({ title }: PageHeadingProps) => {
+export const PageHeading = ({ title, description }: PageHeadingProps) => {
   return (
     <div className="mx-auto text-center pt-3 lg:pt-10 pb-10 lg:pb-20 max-w-5xl">
-      <Heading variant="h1" htmlAttributes={{ className: 'pb-5 uppercase' }}>
+      <Heading variant="h1" htmlAttributes={{ className: ' uppercase' }}>
         {title}
       </Heading>
+      {description && <Paragraph>{description}</Paragraph>}
       <Separator />
     </div>
   );
