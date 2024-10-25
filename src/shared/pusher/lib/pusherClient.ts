aa657f68ddd7f6bc46a582e6cd7184ad2132140f
Sebastian Siejek <siejeksebastian@gmail.com>
2024-10-26 01:10:44 +0200

    refactor: remove pusher auth
    Signed-off-by: Sebastian Siejek <siejeksebastian@gmail.com>

--- src/shared/pusher/lib/pusherClient.ts
+++ src/shared/pusher/lib/pusherClient.ts
@@ -7,8 +7,6 @@ export const pusherClient = () => {
     if (!window.pusherInstance) {
       window.pusherInstance = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
         cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
-        authEndpoint: '/api/pusher-auth', // OPTIONAL: For secure web sockets.
-        authTransport: 'ajax',
       });
     }
 
