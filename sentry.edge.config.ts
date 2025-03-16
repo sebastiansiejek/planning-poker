5d4fb7afcc80dd55c3c01e6a0c240dfd1619695c
Sebastian Siejek <siejeksebastian@gmail.com>
2025-03-16 09:34:25 +0100

    chore(deps): update @sentry/nextjs to v9.5.0 and refactor configuration
    Signed-off-by: Sebastian Siejek <siejeksebastian@gmail.com>

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,16 +3,14 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs";
 
-if (process.env.NODE_ENV === 'production') {
-  Sentry.init({
-    dsn: 'https://8a3916d91d0ba5ef01c183f912c5792a@o4507850206347264.ingest.de.sentry.io/4507862374940752',
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DNS,
 
-    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-    tracesSampleRate: 1,
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
 
-    // Setting this option to true will print useful information to the console while you're setting up Sentry.
-    debug: false,
-  });
-}
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,16 +2,14 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs";
 
-if (process.env.NODE_ENV === 'production') {
-  Sentry.init({
-    dsn: 'https://8a3916d91d0ba5ef01c183f912c5792a@o4507850206347264.ingest.de.sentry.io/4507862374940752',
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DNS,
 
-    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-    tracesSampleRate: 1,
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
 
-    // Setting this option to true will print useful information to the console while you're setting up Sentry.
-    debug: false,
-  });
-}
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
