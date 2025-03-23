4275d572dc811ccaf2c1f153bfe51300001ce6fe
Sebastian Siejek <siejeksebastian@gmail.com>
2025-03-23 11:40:15 +0100

    test(voting): use dynamic room names and standardize quotes in test
    Signed-off-by: Sebastian Siejek <siejeksebastian@gmail.com>

--- tests/voting.spec.ts
+++ tests/voting.spec.ts
@@ -1,17 +1,15 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
-import { databaseTestAuth } from '@/shared/lib/tests/utils';
+import { databaseTestAuth } from "@/shared/lib/tests/utils";
 
 databaseTestAuth();
 
-const ROOM_NAME = 'test-room';
-
-test('voting', async ({ page }) => {
-  await page.goto('/');
-  await page.getByTestId('start-new-game').click();
-  await page.getByTestId('game-name').click();
-  await page.getByTestId('game-name').fill(ROOM_NAME);
-  await page.getByTestId('create-game-submit').click();
+test("voting", async ({ page }) => {
+  await page.goto("/");
+  await page.getByTestId("start-new-game").click();
+  await page.getByTestId("game-name").click();
+  await page.getByTestId("game-name").fill(`test-${Math.random()}`);
+  await page.getByTestId("create-game-submit").click();
 
   await new Promise((resolve) => {
     setTimeout(resolve, 300);
@@ -22,13 +20,13 @@ test('voting', async ({ page }) => {
     .isVisible();
 
   if (isMessageVisible) {
-    await page.getByTestId('join-to-game').click();
+    await page.getByTestId("join-to-game").click();
   }
 
   await page.waitForURL(/\/game\/\w+$/);
-  await page.getByTestId('create-game-trigger-button').click();
-  await page.getByTestId('create-game-submit').click();
-  await page.getByTestId('voting-card-3').click();
-  await page.getByTestId('reveal-cards-button').click();
-  await expect(page.getByTestId('voting-avg')).toBeVisible();
+  await page.getByTestId("create-game-trigger-button").click();
+  await page.getByTestId("create-game-submit").click();
+  await page.getByTestId("voting-card-3").click();
+  await page.getByTestId("reveal-cards-button").click();
+  await expect(page.getByTestId("voting-avg")).toBeVisible();
 });
