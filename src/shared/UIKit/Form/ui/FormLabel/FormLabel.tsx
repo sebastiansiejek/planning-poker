2830ff5aaaf93bee34f704e1421072c4d5858bb0
Sebastian Siejek <siejeksebastian@gmail.com>
2025-03-23 10:06:42 +0100

    refactor: standardize quotes in FormLabel component and improve labelVariants definition
    Signed-off-by: Sebastian Siejek <siejeksebastian@gmail.com>

--- src/shared/UIKit/Form/ui/FormLabel/FormLabel.tsx
+++ src/shared/UIKit/Form/ui/FormLabel/FormLabel.tsx
@@ -1,15 +1,15 @@
-'use client';
+"use client";
 
-import * as LabelPrimitive from '@radix-ui/react-label';
-import { cva, type VariantProps } from 'class-variance-authority';
-import type { ComponentPropsWithoutRef, ElementRef } from 'react';
-import { forwardRef } from 'react';
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { cva, type VariantProps } from "class-variance-authority";
+import type { ComponentPropsWithoutRef, ElementRef } from "react";
+import { forwardRef } from "react";
 
-import { useFormField } from '@/shared/UIKit/Form/model/useFormField/useFormField';
-import { renderClass } from '@/shared/utils/renderClass/renderClass';
+import { useFormField } from "@/shared/UIKit/Form/model/useFormField/useFormField";
+import { renderClass } from "@/shared/utils/renderClass/renderClass";
 
 const labelVariants = cva(
-  'text-sm mb-2 font-medium cursor-pointer leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+  "block text-sm mb-2 font-medium cursor-pointer leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
 );
 
 const FormLabel = forwardRef<
@@ -23,7 +23,7 @@ const FormLabel = forwardRef<
     <LabelPrimitive.Root
       ref={ref}
       className={renderClass(
-        error && 'text-destructive',
+        error && "text-destructive",
         labelVariants(),
         className,
       )}
