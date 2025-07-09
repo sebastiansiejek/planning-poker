2da9e8b8fc22af9ff624403faf3861bbcc55868e
Sebastian Siejek <siejeksebastian@gmail.com>
2025-07-09 20:55:31 +0200

    fix(theme-switcher): ensure component does not render on server-side
    - Added a `mounted` state with `useEffect` to prevent rendering before hydration.
    - Resolved potential flashing issues by returning null during server-side rendering.
    
    Signed-off-by: Sebastian Siejek <siejeksebastian@gmail.com>

--- src/widgets/ThemeSwitcher/ui/theme-switcher.tsx
+++ src/widgets/ThemeSwitcher/ui/theme-switcher.tsx
@@ -2,11 +2,21 @@
 
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 const ICON_SIZE = 14;
 
 export const ThemeSwitcher = () => {
   const { resolvedTheme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <button
