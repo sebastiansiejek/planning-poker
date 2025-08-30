be99a85a6b37dde23a1be28e91f4058292394645
Sebastian Siejek <siejeksebastian@gmail.com>
2025-08-30 13:00:16 +0200

    chore(deps): standardize quotes in `pnpm-lock.yaml`
    - Replaced single quotes with double quotes in dependency keys for consistency.
    - Applied uniform formatting throughout the `pnpm-lock.yaml` file.
    
    Signed-off-by: Sebastian Siejek <siejeksebastian@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,70 +1,63 @@
 import createNextIntlPlugin from "next-intl/plugin";
-import {withSentryConfig} from "@sentry/nextjs";
+import { withSentryConfig } from "@sentry/nextjs";
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "www.gravatar.com",
-                port: "",
-                pathname: "/avatar/**",
-            },
-            {
-                protocol: "https",
-                hostname: "avatars.githubusercontent.com",
-                port: "",
-                pathname: "/u/**",
-            },
-            {
-                protocol: "https",
-                hostname: "*.googleusercontent.com",
-                port: "",
-                pathname: "/**",
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.gravatar.com",
+        port: "",
+        pathname: "/avatar/**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/u/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
-export default withSentryConfig(
-    withNextIntl(nextConfig),
-    {
-        // For all available options, see:
-        // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+export default withSentryConfig(withNextIntl(nextConfig), {
+  // For all available options, see:
+  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-        org: "sebastian-siejek-dev",
-        project: "planning-poker",
+  org: "sebastian-siejek-dev-2a",
 
-        // Only print logs for uploading source maps in CI
-        silent: !process.env.CI,
+  project: "planning-poker",
 
-        // For all available options, see:
-        // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
 
-        // Upload a larger set of source maps for prettier stack traces (increases build time)
-        widenClientFileUpload: true,
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-        // Automatically annotate React components to show their full name in breadcrumbs and session replay
-        reactComponentAnnotation: {
-            enabled: true,
-        },
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
 
-        // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-        // This can increase your server load as well as your hosting bill.
-        // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-        // side errors will fail.
-        tunnelRoute: "/monitoring",
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  tunnelRoute: "/monitoring",
 
-        // Automatically tree-shake Sentry logger statements to reduce bundle size
-        disableLogger: true,
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
 
-        // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-        // See the following for more information:
-        // https://docs.sentry.io/product/crons/
-        // https://vercel.com/docs/cron-jobs
-        automaticVercelMonitors: true,
-    }
-);
+  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+  // See the following for more information:
+  // https://docs.sentry.io/product/crons/
+  // https://vercel.com/docs/cron-jobs
+  automaticVercelMonitors: true,
+});
