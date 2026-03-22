cff1656c6a7b7fc3ffba907d802a9bd1ecd03687
Sebastian Siejek <siejeksebastian@gmail.com>
2026-03-22 10:39:15 +0100

    chore: change turbopack with webpack

--- next.config.mjs
+++ next.config.mjs
@@ -27,17 +27,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    config.ignoreWarnings = [
-      ...(config.ignoreWarnings ?? []),
-      {
-        module: /next-intl[\\/].*extractor[\\/]format[\\/]index\.js$/,
-        message: /Build dependencies behind this expression are ignored and might cause incorrect cache invalidation\./,
-      },
-    ];
-
-    return config;
-  },
 };
 
 export default withSentryConfig(withNextIntl(nextConfig), {
@@ -63,12 +52,16 @@ export default withSentryConfig(withNextIntl(nextConfig), {
   // side errors will fail.
   tunnelRoute: '/monitoring',
 
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
+  webpack: {
+    // Automatically tree-shake Sentry logger statements to reduce bundle size.
+    treeshake: {
+      removeDebugLogging: true,
+    },
 
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
+    // Enables automatic instrumentation of Vercel Cron Monitors.
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
+  },
 });
